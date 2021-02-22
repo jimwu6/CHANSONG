24d0cc0f94c2e8bf30f226e6ad49e48eebd3f6ac
Jim Wu <jim.wu6@outlook.com>
2021-02-22 13:15:57 -0500

    Update check_chars.py

--- scraping/check_chars.py
+++ scraping/check_chars.py
@@ -3,12 +3,8 @@ text = open(path_to_file, 'rb').read().decode(encoding='utf-8')
 
 # CHECK TEXT
 print('Length of text: {} characters'.format(len(text)))
-#print(text[:1000])
 
 # CHECK UNIQUE CHARS
 vocab = sorted(set(text))
 print('{} unique characters'.format(len(vocab)))
 print(vocab)
-# print(text.index(vocab[-1]))
-# print(text[1572670:1572700])
-print()
